468a236d39acf50abc5ff9086e5d2312dd89231c
Nahum Sá <nahumsaa@gmail.com>
2022-06-08 23:26:57 -0300

    add typing

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -1,9 +1,9 @@
 import pickle
 import pandas as pd
 
-from typing import Any, List
+from typing import Any, List, Tuple, Protocol
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
@@ -16,23 +16,59 @@ from prefect import flow, task
 from prefect.task_runners import SequentialTaskRunner
 from prefect.logging import get_run_logger
 
-def save_pickle(save_name: str, object: Any):
-    with open(save_name, 'wb') as file:
+class ScikitModel(Protocol):
+    def fit(self, X, y, sample_weight=None): ...
+    def predict(self, X): ...
+
+
+class PreprocessingMethod(Protocol):
+    def fit(self, X): ...
+    def fit_transform(self, X): ...
+    def transform(self, X): ...
+
+
+def save_pickle(save_path: str, object: Any) -> None:
+    """Save an object to the save_path
+
+    Args:
+        save_path (str): Path to save the object
+        object (Any): object that you want to save
+    """
+    with open(save_path, 'wb') as file:
         pickle.dump(object, file)
 
 @task
-def read_data(path):
+def read_data(path: str) -> pd.DataFrame:
+    """Reads parquet data for a given path
+
+    Args:
+        path (str): path to read
+
+    Returns:
+        pd.DataFrame: parquet data
+    """
     df = pd.read_parquet(path)
     return df
 
 @task
-def prepare_features(df, categorical, train=True):
+def prepare_features(df: pd.DataFrame, categorical: List[str], train: bool=True) -> pd.DataFrame:
+    """ Preprocess the features by creating duration and filtering between 1 and 10 minutes
+
+    Args:
+        df (pd.DataFrame): dataframe to preprocess
+        categorical (List[str]): categorical variables
+        train (bool, optional): True if you are preprocessing the train set. Defaults to True.
+
+    Returns:
+        pd.DataFrame: preprocessed data
+    """
     logger = get_run_logger()
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
     df['duration'] = df.duration.dt.total_seconds() / 60
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     mean_duration = df.duration.mean()
+    
     if train:
         logger.info(f"The mean duration of training is {mean_duration}")
     else:
@@ -42,7 +78,18 @@ def prepare_features(df, categorical, train=True):
     return df
 
 @task
-def train_model(df: pd.DataFrame, categorical: List[str], date: str):
+def train_model(df: pd.DataFrame, categorical: List[str], date: str) -> Tuple[PreprocessingMethod, ScikitModel]:
+    """ Create a dict vectorizer and train the Linear Regression model.
+
+    Args:
+        df (pd.DataFrame): train data
+        categorical (List[str]): categorical variables to select from the data
+        date (str): data that you want to train
+
+    Returns:
+        PreprocessingMethod: DictVectorizer method that was fitted to the train data
+        ScikitModel: LinearRegression model that was fitted on the train data
+    """
     logger = get_run_logger()
     
     train_dicts = df[categorical].to_dict(orient='records')
@@ -64,7 +111,15 @@ def train_model(df: pd.DataFrame, categorical: List[str], date: str):
     return lr, dv
 
 @task
-def run_model(df, categorical, dv, lr):
+def run_model(df: pd.DataFrame, categorical: List[str], dv: PreprocessingMethod, lr: ScikitModel):
+    """ Run the model on a given dataframe
+
+    Args:
+        df (pd.DataFrame): data
+        categorical (List[str]): categorical variables that were used on the train step
+        dv (PreprocessingMethod): Method used to preprocess categorical variables on the train step
+        lr (ScikitModel): model used on the train step
+    """
     logger = get_run_logger()
     val_dicts = df[categorical].to_dict(orient='records')
     X_val = dv.transform(val_dicts) 
@@ -76,12 +131,22 @@ def run_model(df, categorical, dv, lr):
     return
 
 @task
-def get_paths(date: str):
-    logger = get_run_logger()
+def get_paths(date: str) -> Tuple[str, str]:
+    """ Create paths for a given date
+
+    Args:
+        date (str): date in the format "%Y-%m-%d
+
+    Returns:
+        str: path to the train data
+        str: path to the test data
+    """
     if isinstance(date, str):
         base_date = datetime.strptime(date, '%Y-%m-%d')
+    
     else:
         base_date = datetime.now()
+    
     train_date = base_date - relativedelta(months=2)
     val_date = base_date - relativedelta(months=1)
     base_path = lambda _date: f'./data/fhv_tripdata_{_date.year}-{_date.strftime("%m")}.parquet'
@@ -90,7 +155,7 @@ def get_paths(date: str):
     
 
 @flow(task_runner=SequentialTaskRunner())
-def main(date=None):
+def main(date: str=None):
     
     train_path, val_path = get_paths(date).result()
     categorical = ['PUlocationID', 'DOlocationID']
